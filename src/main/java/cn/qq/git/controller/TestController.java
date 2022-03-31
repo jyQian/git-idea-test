992e4bac032645a3374988f56e0f7a24a1096920
zhangliuqian <2256701314@qq.com>
2022-03-31 22:11:05 +0800

    dev第一次提交

--- src/main/java/cn/qq/git/controller/TestController.java
+++ src/main/java/cn/qq/git/controller/TestController.java
@@ -3,5 +3,6 @@ package cn.qq.git.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+        System.out.println("Hello git dev!");
     }
 }
